013a8d234fbc5074e0396460e8d7e6349a5ecaf5
nolan <nericks2@charlotte.edu>
2026-03-18 16:27:26 -0400

    Rating.cs, IRatingRepository.cs, RatingEnum.cs addition

---
+++ Contracts/IRatingRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultNamespace;
+
+public interface IRatingRepository
+{
+    void AddRating(Rating rating);
+    bool RemoveRating(Rating rating);
+    bool UpdateRating(string author, string title, int year);
+    List<Rating> GetAllForMember(int id);
+}
---
+++ Domain/Rating.cs
@@ -0,0 +1,20 @@
+﻿namespace DefaultNamespace;
+
+public class Rating
+{
+    public Book Book { get; set; }
+    public Member Member { get; set; }
+    public RatingEnum RatingValue { get; set; }
+
+    public Rating(Book book, Member member, RatingEnum rating)
+    {
+        Book = book;
+        Member = member;
+        RatingValue = rating;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Book: {Book.Title}, Member: {Member.Name}, Rating: {RatingValue}");
+    }
+}
