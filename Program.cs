f514c3bd4cd2a552963af248992ba39ebb018dcb
Gabe Drewery <gdrewer1@charlotte.edu>
2026-03-22 16:33:58 -0400

    Updated MemberRepo and Member Service.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
+using System.ComponentModel.Design.Serialization;
+
 public class Program {
      public static void Main(string[] args)
     {
 
         IBookRepository BookRepo = new BookRepository();
+        IMemberService memService = new MemberService();
+
+        Console.WriteLine("Welcome to the Book Recommendation Program.");
+
+        int logoutSelection = -1;
+        int loginSelection = -1;
+
+        while (logoutSelection != 4)
+        {
+            if (memService.CurrentUser == null)
+            {
+                Console.WriteLine("");
+            }
+        }
+
+
 
 
 
