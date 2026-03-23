b036cc85d6cb51b71027e9e09a19b3c9735d9215
nolan <nericks2@charlotte.edu>
2026-03-22 20:25:01 -0400

    Fixes to program.cs conflicts

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design.Serialization;
+
+public class Program {
+    public static void Main(string[] args)
+    {
+
+        IBookRepository BookRepo = new BookRepository();
+        IMemberService memService = new MemberService();
+
+        Console.WriteLine("Welcome to the Book Recommendation Program.");
+
+        int logoutSelection = -1;
+        int loginSelection = -1;
+
+        while (logoutSelection != 4)
+        {
+            if (memService.CurrentUser == null)
+            {
+                Console.WriteLine("");
+            }
+        }
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
+}
