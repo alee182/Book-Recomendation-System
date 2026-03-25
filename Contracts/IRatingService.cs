54bd67650c1968b1baba81e4468d6a6e1b403411
nolan <nericks2@charlotte.edu>
2026-03-24 21:38:00 -0400

    Documentation added to all rating related files.

--- Contracts/IRatingService.cs
+++ Contracts/IRatingService.cs
@@ -1,9 +1,34 @@
-﻿
-
+﻿/// <summary>
+/// Interface defining methods for creating, viewing, and generating recommendations
+/// based on ratings in the system.
+/// </summary>
 public interface IRatingService
 {
+    /// <summary>
+    /// Creates a new rating and adds it to the system.
+    /// </summary>
+    /// <param name="bookId">The ID of the book being rated.</param>
+    /// <param name="memberId">The ID of the member who is rating the book.</param>
+    /// <param name="rating">The RatingEnum value the user is rating the book.</param>
+    /// <returns>Returns a boolean if adding the rating was successful or not.</returns>
     bool NewRating(int bookId, int memberId, RatingEnum rating);
+    
+    /// <summary>
+    /// Views the ratings belonging to a given user.
+    /// </summary>
+    /// <param name="memberId">The ID of the member whose ratings we are viewing.</param>
+    /// <returns>Returns a list of the user's ratings.</returns>
     List<Rating> ViewRatings(int memberId);
+    
+    /// <summary>
+    /// Compares the current user to another user in the system.
+    /// </summary>
+    /// <param name="otherMemberId">The ID of the member we are comparing the user with.</param>
+    /// <returns>Returns a dot product value calculated by both users' respective ratings.</returns>
     int CompareTo(int otherMemberId);
+    
+    /// <summary>
+    /// Generates a list of recommended books based on users with similar ratings, and prints that list.
+    /// </summary>
     void GenerateRecommendations();
 }
--- Repositories/RatingRepository.cs
+++ Repositories/RatingRepository.cs
@@ -1,14 +1,26 @@
-﻿public class RatingRepository : IRatingRepository
+﻿/// <summary>
+/// Implementation for the methods defined in the IRatingRepository interface.
+/// Uses a nested dictionary where the keys are as follows:
+/// memberId -> (bookId -> rating)
+/// </summary>
+public class RatingRepository : IRatingRepository
 {
-    // KEYS: memberId -> bookId -> rating
+    //Storage for rating objects, KEYS: memberId -> (bookId -> rating)
     private readonly Dictionary<int, Dictionary<int, Rating>> _ratings;
-        
+    
+    /// <summary>
+    /// Constructor for initializing the repository.
+    /// </summary>
+    /// <param name="ratings">An existing dictionary that can be used to store rating objects.</param>
     public RatingRepository(Dictionary<int, Dictionary<int, Rating>> ratings)
     {
         _ratings = ratings;
     }
     
-
+    /// <summary>
+    /// Adds or updates a rating object in the repository.
+    /// </summary>
+    /// <param name="rating">The rating object to be added.</param>
     public void AddRating(Rating rating)
     {
         int memberId = rating.MemberId;
@@ -21,24 +33,36 @@
             _ratings[memberId] = memberRatings;
         }
 
-        // set if existing.
+        // set if existing
         memberRatings[bookId] = rating;
     }
 
+    /// <summary>
+    /// Removes a rating object from the repository based on keys in the dictionary.
+    /// </summary>
+    /// <param name="memberId">The member ID whose rating we are removing.</param>
+    /// <param name="bookId">The book ID of the book that was rated.</param>
+    /// <returns>Returns a boolean value depending on if rating removal was successful or not.</returns>
     public bool RemoveRating(int memberId, int bookId)
     {
-        //if member doesn't exist, return false.
+        //if member doesn't exist, return false
         if (!_ratings.TryGetValue(memberId, out var memberRatings))
         {
             return false;
         }
 
-        //remove rating.
+        //remove rating
         bool removed = memberRatings.Remove(bookId);
         
         return removed;
     }
 
+    /// <summary>
+    /// Gets a rating object based on the keys of the nested dictionaries.
+    /// </summary>
+    /// <param name="memberId">The ID of the member who's rating we are getting.</param>
+    /// <param name="bookId">The ID of the book that was rated.</param>
+    /// <returns>Returns the rating object if found, otherwise returns null.</returns>
     public Rating? GetRating(int memberId, int bookId)
     {
         //if memberId and bookId found, returns rating.
@@ -51,6 +75,11 @@
         return null;
     }
 
+    /// <summary>
+    /// Gets all ratings for a given member in the system.
+    /// </summary>
+    /// <param name="memberId">The ID of the member whose ratings we are getting.</param>
+    /// <returns>Returns a list of all the given member's rating objects. Returns an empty list if no ratings there.</returns>
     public List<Rating> GetAllForMember(int memberId)
     {
         //if memberId found, return list of ratings
@@ -62,7 +91,11 @@
         //returns empty list
         return new List<Rating>();
     }
-    //needed for usage in ratingservice, ADD TO UML
+    
+    /// <summary>
+    /// Gets all member IDs that are used as keys in the outer dictionary. 
+    /// </summary>
+    /// <returns>Returns a list of all the member IDs in the outer dictionary as integers.</returns>
     public List<int> GetAllMemberIds()
     {
         return _ratings.Keys.ToList();
